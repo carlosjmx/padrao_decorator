df719d5dc2a79b2d6afc71192358854989c5503a
carlinhos xavier <carlinhosjmx7@gmail.com>
2022-10-03 21:49:41 -0300

    criado a classe decorator

---
+++ AcrescimoDecorator.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace padraoDecorator;
+
+abstract class AcrescimoDecorator extends Pizza {
+
+    protected Pizza $pizza;
+
+    public function __construct(Pizza $pizza){ 
+        
+        $this->pizza = $pizza;
+    } 
+
+    abstract public function getDescricao(): string;
+
+    abstract public function getPreco(): float;
+   
+ 
+}
